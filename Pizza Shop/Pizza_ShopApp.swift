54d912fa35ed318da07a8c2944bfab6c81841bd5
Johannes Meissner <johannes.meissner@hse.com>
2023-10-04 17:15:11 +0200

    Initial Commit

---
+++ Pizza Shop/Pizza_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_ShopApp.swift
+//  Pizza Shop
+//
+//  Created by Meißner, Johannes, HSE DE on 04.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
